4e5b49664583b779b0fd54d88cd8a20597e12c8f
weibin02 <weibin5369@gmail.com>
2025-03-15 22:54:56 +0800

    add

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function home(){
+        return view('home')
+    }
+
+    public function about(){
+        return view('about')
+    }
+
+    public function projects() {
+        return view('projects');
+    }
+
+    public function contact() {
+        return view('contact');
+    }
+}
